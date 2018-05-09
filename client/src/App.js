08ed1860ff297af639dd4be878439f11bd864f43
Ray Zhou <zhou.ray@gmail.com>
2018-05-09 12:55:01 -0700

    rename container name

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import MatchStatsContainer from './containers/MatchStatsContainer'
+import MatchStatsTableContainer from './containers/MatchStatsTableContainer'
 import './App.css';
 
 class App extends Component {
-  
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
         </header>
-        <MatchStatsContainer />
+        <MatchStatsTableContainer />
       </div>
     );
   }
